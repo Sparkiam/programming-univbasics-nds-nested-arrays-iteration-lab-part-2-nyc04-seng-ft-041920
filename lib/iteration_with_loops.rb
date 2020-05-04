740cacdf95002244a236cda261106d666fbecef8
Dell Wilson <warren.wilsonjr89@gmail.com>
2020-05-04 20:40:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,8 @@
 
 def find_min_in_nested_arrays(array_of_daily_temperatures)
-    array_of_daily_temps[x]
+    array_of_daily_temperatures[x]
     x += 1
-    array_of_daily_temps[x].min
+    array_of_daily_temperatures[x].min
     
     
 #     small_temp = []
